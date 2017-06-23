9783178f1f1d83782fe454fda511d26e9d22a22e
Gabriel Saruhashi <gabesaruhashi@fb.com>
2017-06-23 11:20:59 -0700

    Core app, without extra optionals

---
+++ app/src/main/java/com/codepath/flicks/models/Movie.java
@@ -0,0 +1,59 @@
+package com.codepath.flicks.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by gabesaruhashi on 6/22/17.
+ */
+
+public class Movie {
+
+    // values from API
+
+    private String title;
+    private String overview;
+    private String posterPath; // only the path
+    private String backdropPath;
+
+    // Initialize from JSON data
+    public Movie(JSONObject object) throws JSONException {
+        title = object.getString("title");
+        overview = object.getString("overview");
+        posterPath = object.getString("poster_path");
+        backdropPath = object.getString("backdrop_path");
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+}
+
